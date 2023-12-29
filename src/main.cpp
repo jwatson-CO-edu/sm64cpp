a357720850fee59f7a5afcd0f037bb8b0421fda7
James Watson <james.watson-2@colorado.edu>
2023-12-29 09:47:46 -0700

    copy entry point as a comment

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+// void main_func(void) {
+//     UNUSED u8 filler[64];
+
+//     osInitialize();
+//     stub_main_1();
+//     create_thread(&gIdleThread, 1, thread1_idle, NULL, gIdleThreadStack + 0x800, 100);
+//     osStartThread(&gIdleThread);
+// }
